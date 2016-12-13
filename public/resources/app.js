87e971a3ddb6cca62695c0ae386010c5fa7425f1
Alex Lerman <alexander.a.lerman@gmail.com>
2016-12-13 15:20:29 -0800

    using https and removed base url

--- public/resources/app.js
+++ public/resources/app.js
@@ -3,7 +3,7 @@ angular.module('OathStructure', ['perfect_scrollbar', 'ui.sortable'])
 
 .config(function ($locationProvider) {
   $locationProvider.html5Mode({
-    enabled: true
-    // requireBase: false
+    enabled: true,
+    requireBase: false
   });
 })
